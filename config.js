ff8deac454fe344feef4127af4a91082726205a9
Amirrr <111552516+Amrbot01@users.noreply.github.com>
2022-09-01 19:51:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,21 +3,21 @@ let d = new Date(new Date + 3600000)
 let locale = 'id'
 let chalk = require('chalk')
 global.owner = [
-  ['6289509960601'],
-  ['6289509960601'],
-  ['6289509960601', 'Deffri', true]  
+  ['6282154156241'],
+  ['6282154156241'],
+  ['6282154156241', 'Amirr', true]  
   
 ] 
-global.noowner = '6289509960601'
-global.ownername = [['𝐃𝐞𝐟𝐟𝐫𝐢 𝐆𝐚𝐧𝐬']]
-global.namabot = [['𝑮𝒂𝒏𝒔𝑩𝑶𝑻𝒛']]
-global.wm2 = [['Hoh']]
+global.noowner = '6282154156241'
+global.ownername = [['Amirr']]
+global.namabot = [['XYZEN BOTZ']]
+global.wm2 = [['XYZEN BOTZ']]
 global.instalu = [['https://instagram.com/b4c00t.dtz']]
 global.ytlu = [['https://youtube.com/channel/UCmEQy5B3GhmfNVSrHEzv-uA']]
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.log =  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=outline-shadow-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.mods = []
-global.prems = ['6289509960601'] 
+global.prems = ['6282154156241'] 
 
 //+$&#-$:$!#(#;#!$(_!#+$;_)_!&
 global.fotonya1 = 'https://telegra.ph/file/3ee98976cb4f784eab41f.jpg' //ganti jadi foto bot mu
@@ -56,11 +56,11 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'Deffri Gans'
-  var sticker_author = 'By GansBOTz'
+  var sticker_name = 'Amirr'
+  var sticker_author = 'By XYZEN BOTZ'
 } else {
-  var sticker_name = '𝐃𝐞𝐟𝐟𝐫𝐢 𝐆𝐚𝐧𝐬'
-  var sticker_author = 'by 𝑮𝒂𝒏𝒔𝑩𝑶𝑻𝒛'
+  var sticker_name = 'Amirr'
+  var sticker_author = 'by XYZEN BOTZ'
 }
 
 const file_exif = "lib/exif.json"
@@ -74,7 +74,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by GansBOTz'
+global.wm = 'Created by XYZEN BOTZ'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
